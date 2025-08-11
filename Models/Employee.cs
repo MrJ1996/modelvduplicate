71e49d6fa54fa97ca19da5fb0f50e669149d2f5d
javed <javed@DESKTOP-KFGGM0G>
2025-08-11 19:09:57 +0530

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿namespace modelvduplicate.Models
+{
+    public class Employee
+    {
+
+        public int ID {get; set;}
+
+        public string FirstName {get; set;}
+
+        public string MiddleName {get; set;}
+
+        public string LastName {get; set;}
+
+        public string PhoneNumber {get; set;}
+
+        public string EmailAddress {get; set;}
+
+        public string Gender { get; set; }
+
+        public string Country { get; set; }
+    }
+}
